76dbcb6d4665b2775ad8c6efd72a58375a404813
Aleksandr Grigorii <domosedov.dev@gmail.com>
2025-10-23 15:31:06 +0300

    try fix better auth

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,19 +1,20 @@
-import { betterAuth } from 'better-auth'
-import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-import { openAPI } from 'better-auth/plugins'
-import { db } from '~/db'
-import * as authSchema from '~/db/schema/auth'
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI } from "better-auth/plugins";
+import { db } from "~/db";
+import * as authSchema from "~/db/schema/auth";
 
 export const auth = betterAuth({
-	basePath: '/',
-	database: drizzleAdapter(db, {
-		provider: 'pg',
-		usePlural: true,
-		debugLogs: false,
-		schema: authSchema,
-	}),
-	plugins: [openAPI()],
-	emailAndPassword: {
-		enabled: true,
-	},
-})
+  basePath: "/",
+  trustedOrigins: ["http://localhost:3000", "https://txqtv.ru"],
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+    debugLogs: false,
+    schema: authSchema,
+  }),
+  plugins: [openAPI()],
+  emailAndPassword: {
+    enabled: true,
+  },
+});
